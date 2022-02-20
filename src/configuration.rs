c3528e7eb642ca4efd233dda3ab137100ab6db8e
lassebloch <lassebloch@gmail.com>
2022-02-20 12:52:48 +0100

    Added output_file to Configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,18 +1,65 @@
+use regex::Regex;
+use std::ops::Add;
+
 #[derive(Debug)]
 pub struct Configuration {
     convert_gcode: bool,
     input_file: String,
+    output_file: String,
 }
 
 impl Configuration {
-    pub fn new(convert_gcode: bool, input_file: String) -> Configuration {
+    pub fn new(
+        convert_gcode: bool,
+        input_file: String,
+        output_file: std::option::Option<&str>,
+    ) -> Configuration {
         Configuration {
             convert_gcode: convert_gcode,
-            input_file: input_file,
+            input_file: input_file.clone(),
+            output_file: match output_file {
+                Some(file_name) => Configuration::gen_output_filename(file_name),
+                // TODO: add logic to configure output name
+                None => Configuration::gen_output_filename(&input_file),
+            },
+        }
+    }
+
+    fn gen_output_filename(input_filename: &str) -> String {
+        // look for gcode file extension
+        let re = Regex::new(r"\.(GCODE|gcode)$").unwrap();
+        if re.is_match(input_filename) {
+            return String::from(re.replace(input_filename, "_converted.gcode"));
+        } else {
+            return String::from(input_filename).add("_converted.gcode");
         }
     }
 
     pub fn input_file(&self) -> &str {
         return &self.input_file;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_output_file_gen_filename_with_gcode_extension() {
+        assert_eq!(
+            Configuration::gen_output_filename("thing.gcode"),
+            "thing_converted.gcode"
+        );
+        assert_eq!(
+            Configuration::gen_output_filename("thing.GCODE"),
+            "thing_converted.gcode"
+        );
+    }
+
+    #[test]
+    fn test_output_file_gen_filename_without_gcode_extension() {
+        assert_eq!(
+            Configuration::gen_output_filename("thing.exe"),
+            "thing.exe_converted.gcode"
+        );
     }
 }
--- src/line_processor.rs
+++ src/line_processor.rs
@@ -1,9 +1,5 @@
 use regex::Regex;
 
-pub fn hello_world() {
-    println!("blah")
-}
-
 pub fn process_line(s: &mut String) -> String {
     let s = reformat_gcode(s);
     return s;
@@ -26,5 +22,10 @@ mod tests {
         assert_eq!(g0x_to_gx(&mut String::from("G01")), "G1");
         assert_eq!(g0x_to_gx(&mut String::from("G01")), "G1");
         assert_eq!(g0x_to_gx(&mut String::from("G02")), "G2");
+    }
+
+    #[test]
+    fn test_g0x_to_gx_line_without_gcode() {
+        assert_eq!(g0x_to_gx(&mut String::from("Hello world")), "Hello world");
     }
 }
